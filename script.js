2dfd003b3840779e6ccf201b1f97e50641e49568
manas140 <manasbhosale.14@gmail.com>
2023-03-11 15:23:23 +0000

    better script

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', load);
-
-var login = document.querySelector('.login');
-var main = document.querySelector('.main');
-var save = document.querySelector('.save');
+  
+const login = document.querySelector('.login');
+const main = document.querySelector('.main');
+const save = document.querySelector('.save');
 const input = document.querySelector('.input');
-var user = null;
-var called = false;
-var first = null;
+let called = false;
+let first = false;
 
 let Followers;
 if (localStorage.getItem("Followers") === null) {
@@ -16,6 +15,7 @@ if (localStorage.getItem("Followers") === null) {
   Followers = JSON.parse(localStorage.getItem("Followers"));
 }
 
+save.addEventListener('click', log_in)
 input.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
@@ -23,65 +23,91 @@ input.addEventListener("keypress", function(event) {
   }
 }); 
 
-save.addEventListener('click', log_in)
-
 function log_in() {
-  localStorage.setItem('user', input.value);
-  main.style.display = 'grid';
-  load();
+  fetch('https://api.github.com/users/' + input.value)
+    .then( res => { 
+      if ( !res.ok ) {
+        return Promise.reject(res.status);
+      }
+      else {
+        localStorage.setItem('user', input.value);
+        load();
+      }
+    })
+    .catch(err => { err(err); })
 }
 
 function load() {
-  const user = localStorage.getItem('user');
+  const user = localStorage.getItem('user')
   if (user != null) {
     login.style.display = 'none';
-    fetch('https://api.github.com/users/' + user)
-      .then(res => res.json())
-      .then(u_info => {
-        total = u_info.followers;
-        fetch('https://api.github.com/users/' + user + '/followers?per_page=' + total)
-          .then(res => res.json())
-          .then(data => {
-            var i = 0;
-            var followers = [];
-            while (i < total) {
-              followers.push(data[i].login);
-              i++;
-            };
-            console.log(followers, Followers);
-            i = 0;
-            while (i < total) {
-              if (Followers.includes(followers[i]) == false) {
-                create_card(followers[i], 'Followed');
-                Followers.push(followers[i]);
-              }
-              else if (followers.includes(Followers[i]) == false && Followers[i] != null) {
-                create_card(Followers[i], 'UnFollowed');
-                Followers.splice(Followers.indexOf(Followers[i]), 1);
-              }
-              i++;
-              localStorage.setItem("Followers", JSON.stringify(Followers));
-            };
-            if (called == false) {
-              main.innerHTML += `
-                <div class='dialog'>
-                  <p>You've Got No New Updates<br><br>Visit Again Later!</p>
-                </div>
-              `
-            }
-            if (first == true) {
-              main.innerHTML += `
-                <div class='dialog'>
-                  <p>User & Followers List Has Been Saved For Later Use<br><br>Visit Again Later</p>
-                </div>
-              `
-            }
-          })
-      });
+    main.style.display = 'grid';
+    update(user)
   }
-  else {
-    main.style.display = 'none';
-  };
+}
+
+async function update(user) {
+  let page_num = 1;
+  let start = false;
+
+  while (true) {
+    const response = await fetch('https://api.github.com/users/' + user + '/followers?page=' + page_num + '&per_page=100') 
+    if (!response.ok) {
+      err(response.status);
+      break;
+    }
+    
+    const data = await response.json();
+    if ( data.length == 0 ) { 
+      break;
+    }
+
+    const followers = [];
+    for (let i = 0; i < data.length; i++) {
+      followers.push(data[i].login);
+    }
+
+    let i = 0;
+    while (followers.length >= i) {
+      if (Followers.includes(followers[i]) == false && followers[i] != null) {
+        create_card(followers[i], 'Followed');
+        Followers.push(followers[i]);
+      }
+      else if (followers.includes(Followers[i]) == false && Followers[i] != null) {
+        create_card(Followers[i], 'UnFollowed');
+        Followers.splice(Followers.indexOf(Followers[i]), 1);
+      }
+      localStorage.setItem("Followers", JSON.stringify(Followers));
+      start = true;
+      i++;
+    }
+    page_num += 1;
+  }
+
+  if (start == true && called == false ) {
+    main.innerHTML = `
+      <div class='dialog'>
+        <p>You've Got No New Updates<br><br>Visit Again Later!</p>
+      </div>
+    `
+  }
+  else if (start == true && first == true) {
+    main.innerHTML = `
+      <div class='dialog'>
+        <p>User & Followers List Has Been Saved For Later Use<br><br>Visit Again Later!</p>
+      </div>
+    `
+  }
+}
+
+function err(err) {
+  login.style.display = 'none';
+  main.style.display = 'grid';
+  main.innerHTML = `
+    <div class='dialog'>
+      <p>Error: ${err}, Please Try Again Later<br><br>Though If Issue Persists Please Report It.</p>
+    </div>
+  `;
 }
 
 function create_card(name, status) {
@@ -96,6 +122,5 @@ function create_card(name, status) {
       </a>
     `;
   }
-
   called = true
 }
